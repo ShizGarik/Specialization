0f71bc5bfab54b218e9e48526fcfd6b88afa3da9
Dmitry Goncharenko <d.goncharenko1@gmail.com>
2023-02-24 16:32:57 +0300

    Тело программы

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+// меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+// Пример:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
+"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
+
+string[] result = ArrayResult(array, 3);
+
+string[] ArrayResult(string[] input, int n)
+{
+    string[] output = new string[CountArray(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountArray(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"{string.Join(", ", result)}");
